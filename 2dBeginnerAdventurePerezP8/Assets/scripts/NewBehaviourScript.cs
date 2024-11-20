538beebe4fa89ffef38217c670acda66aa5f93ad
Evan <Donoyster@icloud.com>
2024-11-20 15:24:06 -0800

    maweah maewah
    Shaabedi ca de de dee doo dooough

--- 2dBeginnerAdventurePerezP8/Assets/scripts/NewBehaviourScript.cs
+++ 2dBeginnerAdventurePerezP8/Assets/scripts/NewBehaviourScript.cs
@@ -17,8 +17,8 @@ public class NewBehaviourScript : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime ;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
         
         transform.position = position;
 
